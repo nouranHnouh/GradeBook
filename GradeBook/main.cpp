182d784bef32a6091f05724988f3e136ef25a232
Nouran Nouh <nourannouh@Nourans-MacBook-Air.local>
2019-03-10 19:59:06 -0400

    create course name datamember to save the name of the course

--- GradeBook/main.cpp
+++ GradeBook/main.cpp
@@ -8,26 +8,54 @@
 // create GradeBook object and call displayMessage functions
 
 #include <iostream>
+#include<string>
 using namespace std;
 
 
 
 class GradeBook
     {
+    
     public:
+        //constructor initializates cpourseName with string
+        explicit GradeBook(string name)
+        : courseName(name)//initialize course name
+        {
+        }
+        
+        // function setCourseName sets the course name to courseName
+        void setCourseName(string name) {
+            // store course name
+            courseName=name;
+        }
+        string getCourseName() const {
+            return courseName;
+        }
+        
         void displayMessage() const
         {
-            cout<<"Welcome to Grade Book "<<endl;
+            
+            cout<<"Welcome to Grade Book for \n "<< getCourseName()<<"!"<<endl;
             
         }//end function displayMessage
-        
+    private:
+        string courseName;
     }; //end class GradeBook
 
 int main()
 {
-    
+    string courseName;
     // create GradeBook object
     GradeBook gradebook;
+    
+    //dipslay intial course name
+    cout<<"intial course name is : "<<gradebook.getCourseName() <<endl;
+    
+    //ask user to enter course name
+    cout<<"Please enter the name of the course: "<<endl;
+    getline(cin,courseName);
+    gradebook.setCourseName(courseName); //set the course name
+    cout<<endl;
     gradebook.displayMessage();
 
 }//end main 
